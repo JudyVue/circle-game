b8e47cf17be950cae1ff71f90f211186b394f521
Judy Vue <jvue@blulinksolutions.com>
2018-01-05 17:47:10 -0800

    first commit

---
+++ src/Components/Circle/circle.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './circle.css';
+
+
+
+class Circle extends Component {
+    // constructor(props) {
+    //     super(props);
+ 
+    // }
+
+    render() {
+        return (
+            <div className="circle">
+                
+            </div>
+        )
+    }
+}
+
+export default Circle;
