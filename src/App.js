a7fa86513f3d4253120e8da3154dd4b3db6a71c6
Judy Vue <jvue@blulinksolutions.com>
2018-01-08 09:21:50 -0800

    deploying from staging

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import Circle from './Components/Circle/circle'
+import Circle from './Components/Circle/circle';
+import StartButton from './Components/StartButton/startbutton';
 
 // <div className="App">
 // <header className="App-header">
@@ -17,22 +18,57 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-        top: `${0}em`,
+    const declarativeLoop = (num) => {
+      let arr = [];
+      for (let i = 1; i <= num; i++) {
+        arr.push(i);
+      }
+      return arr;
     }
+
+    this.state = {
+        top: 0,
+        units: 'em',
+        circles: [],
+        circlesCount: declarativeLoop(4),
+    }   
     
+    this.dropCircles = this.dropCircles.bind(this);
+
 
   }
   render() {
     return (
-      <div className="circle-game">
-        <Circle 
-        top={this.state.top}
+      <div className="App circle-game">
+        { this.state.circlesCount.map((num, i) => (
+            <Circle top={this.state.top + this.state.units} key={i}/>
+          )
+        )}
+        <StartButton 
+          dropCircles={this.dropCircles}
         />
-        <h1>Hello World, this is my circle game</h1>
       </div>
     );
   }
+
+
+ 
+
+ 
+  dropCircles() {
+      setInterval(() => {
+        this.setState((prevState) => {
+            // console.log(prevState)
+            return {top: prevState.top += 1}
+        })
+        console.log(this.state)
+        // console.log('inside setInterval?')
+
+      }, 500)
+
+  }
+
+
 }
 
 export default App;
