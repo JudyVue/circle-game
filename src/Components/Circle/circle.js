26092ee5bc6c7e2cc8bd8e0ef51aa2f7de567e71
Judy Vue <jvue@blulinksolutions.com>
2018-02-09 22:04:18 -0800

    pushing staging to heroku

--- src/Components/Circle/circle.js
+++ src/Components/Circle/circle.js
@@ -28,7 +28,7 @@ class Circle extends Component {
 
     checkForResetButtonClick(nextProps) {
         if (nextProps.resetCircle) {
-            console.log('in here?', nextProps)
+            // console.log('in here?', nextProps)
             clearInterval(this._dropCircle);
             this.setState({
                 styles: {
@@ -51,9 +51,8 @@ class Circle extends Component {
   
 
     dropCircle(nextProps) {
-        console.log(nextProps, 'inside dropCircle')
+        // console.log(nextProps, 'inside dropCircle')
         if (nextProps.shouldCircleFall) {
-            console.log('hello')
             let newTop = nextProps.top;
             this._dropCircle = setInterval(() => {
                this.setState(prevState => {
